8c6d0be7cbb935bd0dc05fac8330dc2c35fb964d
shikhin <shikhin@metronlabs.com>
2021-10-04 13:40:00 +0530

    added field for url and made necessary changes for it to work

--- DataConnectors/Digital shadows/DigitalShadowsConnectorAzureFunction/constant.py
+++ DataConnectors/Digital shadows/DigitalShadowsConnectorAzureFunction/constant.py
@@ -3,4 +3,3 @@ DAYS = 5
 MINUTE = 15                                 #buffer time for current time
 SHARE_NAME = 'funcstatemarkershare'
 FILE_PATH = 'funcstatemarkerfile'
-URL = "https://api.searchlight.app/v1/"
